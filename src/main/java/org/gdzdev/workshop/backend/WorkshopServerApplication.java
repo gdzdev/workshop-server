848d11a6e202c80df5b4b99a0a684bbf45b8557f
Aleks Galdamez <aleksgaldamez2002@gmail.com>
2025-02-27 14:53:51 -0600

    starting project

---
+++ src/main/java/org/gdzdev/workshop/backend/WorkshopServerApplication.java
@@ -0,0 +1,13 @@
+package org.gdzdev.workshop.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/gdzdev/workshop/backend/WorkshopServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gdzdev.workshop.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
