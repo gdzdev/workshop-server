1754d21a10d3a9fb221e032b63cd7c706ce66e06
Aleks Galdamez <aleksgaldamez2002@gmail.com>
2025-03-03 10:06:19 -0600

    feat: creating entity class

---
+++ src/main/java/org/gdzdev/workshop/backend/inventory/infrastructure/entities/CategoryEntity.java
@@ -0,0 +1,39 @@
+package org.gdzdev.workshop.backend.inventory.infrastructure.entities;
+
+import lombok.*;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "categories")
+public class CategoryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "category")
+    private List<ProductEntity> products = new ArrayList<>();
+}
