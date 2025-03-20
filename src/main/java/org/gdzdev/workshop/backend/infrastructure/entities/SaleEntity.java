0e3ed353e6f5448f7b81877f8c849cd6ec22544a
Aleks Galdamez <aleksgaldamez2002@gmail.com>
2025-03-19 21:24:10 -0600

    feat(sales): creating entities class

---
+++ src/main/java/org/gdzdev/workshop/backend/infrastructure/entities/SaleEntity.java
@@ -0,0 +1,35 @@
+package org.gdzdev.workshop.backend.infrastructure.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.gdzdev.workshop.backend.domain.model.PaymentStatus;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "sales")
+public class SaleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "customer_name")
+    private String customerName;
+
+    @Column(name = "sale_date")
+    private LocalDateTime saleDate;
+
+    private BigDecimal grandTotal;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus paymentStatus;
+
+    @OneToMany(mappedBy = "sale", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<SaleDetailEntity> saleDetails;
+}
